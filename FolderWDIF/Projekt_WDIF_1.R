1e8b94df2ac5a8973e316266ea12e575c5fb8127
Atarios17 <83716600+Atarios17@users.noreply.github.com>
2021-05-05 17:49:30 +0200

    Update Projekt_WDIF_1.R

--- FolderWDIF/Projekt_WDIF_1.R
+++ FolderWDIF/Projekt_WDIF_1.R
@@ -5,10 +5,10 @@ EOP <- function(S0,r,K,Time,dt,sigma){
   n <- Time/dt
   p <- (exp(r*Time))/(u-d)
   
-  Suma = 0
+  Suma <- 0
   
   for(j in 0:n){
-    Suma = Suma + choose(n,j)p^j*(1-p)^(n-j)*max(S0*u^j*d^(n-j)-K,0)      
+    Suma <- Suma + choose(n,j)p^j*(1-p)^(n-j)*max(S0*u^j*d^(n-j)-K,0)      
   }
   
   return(Suma)
